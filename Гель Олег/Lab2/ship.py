cf2ac1439c72fe1556d35433425702eff1f555f1
Heloleh <115833986+Heloleh@users.noreply.github.com>
2023-12-07 11:27:04 +0200

    Add files via upload

---
+++ Гель Олег/Lab2/ship.py
@@ -0,0 +1,29 @@
+from typing import List
+from containers import Container  # Import the Container class from containers.py
+from port import Port
+class Ship:
+    def __init__(self, ID, fuel, currentPort, totalWeightCapacity, maxNumberOfAllContainers, maxNumberOfHeavyContainers,
+                 maxNumberOfRefrigeratedContainers, maxNumberOfLiquidContainers, fuelConsumptionPerKM):
+        self.ID = ID
+        self.fuel = fuel
+        self.currentPort = currentPort
+        self.totalWeightCapacity = totalWeightCapacity
+        self.maxNumberOfAllContainers = maxNumberOfAllContainers
+        self.maxNumberOfHeavyContainers = maxNumberOfHeavyContainers
+        self.maxNumberOfRefrigeratedContainers = maxNumberOfRefrigeratedContainers
+        self.maxNumberOfLiquidContainers = maxNumberOfLiquidContainers
+        self.fuelConsumptionPerKM = fuelConsumptionPerKM
+
+    def getCurrentContainers(self) -> List[Container]:
+        return []  # Replace with actual implementation
+
+
+port1 = Port(1, 40.7128, -74.0060)  # New York Port
+ship1 = Ship(1, 1000, port1, 5000, 10, 5, 3, 2, 0.2)
+
+
+
+current_containers = ship1.getCurrentContainers()
+for container in current_containers:
+    print(f"Container {container.ID}: Type - {container.type}")
+
