41e264674d0dd389dc57d99ce867efa8fd04438f
siriknikita <mykyta.siryk@lnu.edu.ua>
2023-12-07 23:39:04 +0200

    Update on structure of classes. Added dataclasses.

--- Сірик Микита/Lab8/classes/Professor.py
+++ Сірик Микита/Lab8/classes/Professor.py
@@ -1,7 +1,23 @@
-from Person import Person
+from classes.Person import Person, PersonInfo
+from classes.AssessVisitor import AssessVisitor
+from classes.Student import Student
 
 
 class Professor(Person):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, person_info: PersonInfo, research_score: dict, academic_score: int) -> None:
+        super().__init__(person_info)
+        self.research_score = research_score
+        self.academic_score = academic_score
+
+    def accept(self, assess_visitor: AssessVisitor) -> None:
+        # TODO: Implement accepting Visitor
+        pass
+
+    def perform_research(self) -> None:
+        # TODO: Implement research logic
+        pass
+
+    def conduct_classes(self, students: list[Student]) -> None:
+        for student in students:
+            student.perform_studying()
--- Сірик Микита/Lab8/classes/Student.py
+++ Сірик Микита/Lab8/classes/Student.py
@@ -1,7 +1,31 @@
-from classes.Person import Person
+from classes.AssessVisitor import AssessVisitor
+from classes.Person import Person, PersonInfo
+from dataclasses import dataclass
+
+
+@dataclass
+class StudentInfo:
+    research_score: dict
+    academic_score: int
+    visited_lectures: int
 
 
 class Student(Person):
 
-    def __init__(self, id: int, first_name: str, second_name: str, role):
-        super().__init__(id, first_name, second_name, role)
+    def __init__(self, person_info: PersonInfo, student_info: StudentInfo) -> None:
+        super().__init__(person_info)
+        self.research_score = student_info.research_score
+        self.academic_score = student_info.academic_score
+        self.visited_lectures = student_info.visited_lectures
+
+    def accept(self, assess_visitor: AssessVisitor) -> None:
+        # TODO: Implement accepting Visitor
+        pass
+
+    def perform_search(self, tasks: dict) -> None:
+        # TODO: Implement research logic
+        pass
+
+    def perform_studying(self) -> None:
+        # TODO: Implement studying logic
+        pass
