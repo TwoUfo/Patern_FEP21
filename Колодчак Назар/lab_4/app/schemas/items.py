9fe6b3772eaf4b415ec464a81f09a05a5fa5eba0
Nazar7070 <116301073+Nazar7070@users.noreply.github.com>
2023-12-14 11:07:50 +0200

    Update items.py

--- Колодчак Назар/lab_4/app/schemas/items.py
+++ Колодчак Назар/lab_4/app/schemas/items.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from pydantic import BaseModel
 
-
 class Item(BaseModel, ABC):
 
     id: int
@@ -11,29 +10,25 @@ class Item(BaseModel, ABC):
     container_id: int
 
     @abstractmethod
-    def get_total_weight(self):
+    def get_total_weight(self) -> int:
         pass
-
 
 class SmallItem(Item):
 
-    def get_total_weight(self):
+    def get_total_weight(self) -> int:
         return self.weight * self.count
-
 
 class HeavyItem(Item):
 
-    def get_total_weight(self):
+    def get_total_weight(self) -> int:
         return self.weight * self.count
-
 
 class RefrigeratedItem(Item):
 
-    def get_total_weight(self):
+    def get_total_weight(self) -> int:
         return self.weight * self.count
-
 
 class LiquidItem(Item):
 
-    def get_total_weight(self):
+    def get_total_weight(self) -> int:
         return self.weight * self.count
