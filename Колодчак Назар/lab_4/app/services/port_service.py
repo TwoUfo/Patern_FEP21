6155d7acc2cccec35e179805ee135e02f34cde8e
Nazar7070 <116301073+Nazar7070@users.noreply.github.com>
2023-12-14 11:13:29 +0200

    Update port_service.py

--- Колодчак Назар/lab_4/app/services/port_service.py
+++ Колодчак Назар/lab_4/app/services/port_service.py
@@ -1,7 +1,6 @@
 from app.schemas.port import Port
 
-
 class PortFactory:
     @staticmethod
-    def create_port(port_data):
+    def create_port(port_data: dict) -> Port:
         return Port(**port_data)
