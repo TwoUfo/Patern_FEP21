f0e74a8cbfc4a41e0b4dca98856bb566f5cdae19
Nazar7070 <116301073+Nazar7070@users.noreply.github.com>
2023-12-14 11:13:55 +0200

    Update ship_service.py

--- Колодчак Назар/lab_4/app/services/ship_service.py
+++ Колодчак Назар/lab_4/app/services/ship_service.py
@@ -1,16 +1,17 @@
 from app.schemas.ships import LightWeightShip, MediumWeightShip, HeavyWeightShip
-
+from typing import Union, Dict, Any
 
 class ShipFactory:
     @staticmethod
-    def create_ship(ship_data):
+    def create_ship(ship_data: Dict[str, Any]) -> Union[LightWeightShip, MediumWeightShip, HeavyWeightShip]:
         total_weight_capacity = ship_data.get('total_weight_capacity')
 
-        if total_weight_capacity <= 2000:
-            return LightWeightShip(**ship_data)
-        elif total_weight_capacity <= 4000:
-            return MediumWeightShip(**ship_data)
-        elif total_weight_capacity <= 6000:
-            return HeavyWeightShip(**ship_data)
-        else:
-            raise ValueError(f"Invalid ship total weight capacity: {total_weight_capacity}")
+        if total_weight_capacity is not None:
+            if total_weight_capacity <= 2000:
+                return LightWeightShip(**ship_data)
+            elif total_weight_capacity <= 4000:
+                return MediumWeightShip(**ship_data)
+            elif total_weight_capacity <= 6000:
+                return HeavyWeightShip(**ship_data)
+
+        raise ValueError(f"Invalid ship total weight capacity: {total_weight_capacity}")
