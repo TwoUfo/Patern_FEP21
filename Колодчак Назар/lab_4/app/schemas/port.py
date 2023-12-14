e023c6ac2d60e8e10dd74fd57253b9fa2bcc1f75
Nazar7070 <116301073+Nazar7070@users.noreply.github.com>
2023-12-14 11:08:35 +0200

    Update port.py

--- Колодчак Назар/lab_4/app/schemas/port.py
+++ Колодчак Назар/lab_4/app/schemas/port.py
@@ -3,33 +3,25 @@ from typing import List
 from pydantic import BaseModel
 import haversine as hs
 
-from app.schemas.containers import *
-from app.schemas.items import *
-
+from app.schemas.containers import Container
+from app.schemas.items import Item
 
 class IPort(BaseModel, ABC):
-    """Basic abstraction"""
-
     id: int
 
     @abstractmethod
-    def incoming_ship(self, ship):
+    def incoming_ship(self, ship_id: int) -> None:
         pass
 
     @abstractmethod
-    def outgoing_ship(self, ship):
+    def outgoing_ship(self, ship_id: int) -> None:
         pass
 
     @abstractmethod
-    def load_item_to_container(self, item: Item, container: Container):
+    def load_item_to_container(self, item: Item, container: Container) -> None:
         pass
-
-    class Config:
-        from_attributes = True
-
 
 class Port(IPort):
-    """Implements port logic"""
     latitude: float
     longitude: float
     title: str
@@ -42,11 +34,11 @@ class Port(IPort):
         dist = hs.haversine((self.latitude, self.longitude), (port_to_go.latitude, port_to_go.longitude))
         return dist
 
-    def incoming_ship(self, ship_id) -> None:
+    def incoming_ship(self, ship_id: int) -> None:
         if ship_id not in self.current_ships:
             self.current_ships.append(ship_id)
 
-    def outgoing_ship(self, ship_id) -> None:
+    def outgoing_ship(self, ship_id: int) -> None:
         if ship_id in self.current_ships:
             self.current_ships.remove(ship_id)
             self.ship_history.append(ship_id)
@@ -56,10 +48,7 @@ class Port(IPort):
             if container.weight + item.get_total_weight() <= container.get_max_weight():
                 container.items.append(item)
                 self.items.remove(item)
-                # return '<-This Item was loaded in Container->'
             else:
-                pass
-                # return '<-This Item to heavy to load in Container->'
+                pass  # <-This Item is too heavy to load in Container->
         else:
-            pass
-            # return '<-Item and Container are in different Ports->'
+            pass  # <-Item and Container are in different Ports->
