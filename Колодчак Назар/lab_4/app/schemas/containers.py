8910c6284d0fbea838bbb711187d135558a06c35
Nazar7070 <116301073+Nazar7070@users.noreply.github.com>
2023-12-14 11:07:12 +0200

    Update containers.py

--- Колодчак Назар/lab_4/app/schemas/containers.py
+++ Колодчак Назар/lab_4/app/schemas/containers.py
@@ -1,16 +1,15 @@
 from abc import ABC, abstractmethod
-from typing import List, TYPE_CHECKING
+from typing import List
 from pydantic import BaseModel
 
 from app.schemas.items import Item
-
 
 class Container(BaseModel, ABC):
 
     id: int
     title: str
     weight: int
-    _max_weight = int
+    _max_weight: int
     items: List[Item] = []
 
     @abstractmethod
@@ -21,14 +20,10 @@ class Container(BaseModel, ABC):
         id_check = self.id == other.id
         weight_check = self.weight == other.weight
         type_check = self.__class__ == other.__class__
-        if id_check and weight_check and type_check:
-            return True
-        else:
-            return False
+        return id_check and weight_check and type_check
 
     def get_max_weight(self) -> int:
         return self._max_weight
-
 
 class BasicContainer(Container):
 
@@ -37,7 +32,6 @@ class BasicContainer(Container):
     def consumption(self) -> float:
         return self.weight * 2.5
 
-
 class HeavyContainer(Container):
 
     _max_weight = 5000
@@ -45,14 +39,12 @@ class HeavyContainer(Container):
     def consumption(self) -> float:
         return self.weight * 3.0
 
-
 class RefrigeratedContainer(HeavyContainer):
 
     _max_weight = 7000
 
     def consumption(self) -> float:
         return self.weight * 5.0
-
 
 class LiquidContainer(HeavyContainer):
 
